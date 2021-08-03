ef938b3e8dfd2514f4854feebc232acb79c9dd94
Jahangir Alam <ja.dakghor@gmail.com>
2021-08-03 12:26:12 +0600

    initial commit
    Signed-off-by: Jahangir Alam <ja.dakghor@gmail.com>

---
+++ JavaStream/src/Movie.java
@@ -0,0 +1,13 @@
+
+public class Movie {
+	String title;
+	int likes;
+	
+	public int getLikes() {
+		return likes;
+	}
+	public Movie(String title, int likes) {
+		this.title = title;
+		this.likes = likes;
+	}	
+}
